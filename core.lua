274a14665abb33c32d702ec5641e02b2357858d8
Joe Farrelly <joefarrelly96@gmail.com>
2026-06-30 22:50:30 +0100

    Capture current keystone, weekly vault progress, and played time

--- core.lua
+++ core.lua
@@ -9,8 +9,15 @@ local templateSavedVar = {
 function events.ADDON_LOADED(_, name)
     fs:Initialize(name)
 end
+function events.PLAYER_LOGIN(_)
+    RequestTimePlayed()
+end
 function events.PLAYER_LOGOUT(_)
     fs:UpdateAlt()
+end
+function events.TIME_PLAYED_MSG(_, totalTime, levelTime)
+    alt.playedTimeTotal = totalTime
+    alt.playedTimeLevel = levelTime
 end
 
 frame:SetScript("OnEvent", function(self, event, ...)
@@ -60,6 +67,34 @@ function fs.UpdateAlt(_)
     alt.gold = GetMoney()
     fs:UpdateCurrencies()
     fs:UpdateLockouts()
+    fs:UpdateKeystone()
+    fs:UpdateVault()
+end
+
+function fs.UpdateKeystone(_)
+    alt.keystoneMapID = C_MythicPlus.GetOwnedKeystoneChallengeMapID()
+    alt.keystoneLevel = C_MythicPlus.GetOwnedKeystoneLevel()
+end
+
+function fs.UpdateVault(_)
+    local vault = {}
+    local thresholdTypes = {
+        Enum.WeeklyRewardChestThresholdType.Activities,
+        Enum.WeeklyRewardChestThresholdType.Raid,
+        Enum.WeeklyRewardChestThresholdType.RankedPvP,
+    }
+    for _, thresholdType in ipairs(thresholdTypes) do
+        for _, activity in ipairs(C_WeeklyRewards.GetActivities(thresholdType)) do
+            vault[#vault + 1] = {
+                type = thresholdType,
+                index = activity.index,
+                threshold = activity.threshold,
+                progress = activity.progress,
+                level = activity.level,
+            }
+        end
+    end
+    alt.vault = vault
 end
 
 function fs.UpdateCurrencies(_)
