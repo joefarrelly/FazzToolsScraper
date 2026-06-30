5a708d48ade7a9a49671175f4089ffdc8f3e572a
Joe Farrelly <joefarrelly96@gmail.com>
2026-06-30 20:58:39 +0100

    Remove keybind/spec scraping
    Drops fs.SpecScan (action bars, keybind map, spells, macros, items,
    talents) and the addon-detection block it depended on (Dominos/
    Bartender/ElvUI), along with the PLAYER_SPECIALIZATION_CHANGED event
    that triggered it. PLAYER_LOGOUT and ridingSkill tracking are
    untouched. Initialize now just sets up the per-alt SavedVariables
    entry, ready for future addon-only data (gold, currencies,
    lockouts).

--- core.lua
+++ core.lua
@@ -12,9 +12,6 @@ end
 function events.PLAYER_LOGOUT(_)
     fs:UpdateAlt()
 end
-function events.PLAYER_SPECIALIZATION_CHANGED(_)
-    fs:SpecScan()
-end
 
 frame:SetScript("OnEvent", function(self, event, ...)
     events[event](self, ...); -- call one of the functions above
@@ -37,25 +34,6 @@ function fs.Initialize(_, name)
         FazzToolsScraperDB.alts[altKey] = alt
 
         alt.ridingSkill = 0
-        alt.kb = alt.kb or {}
-        alt.kbConfig = alt.kbConfig or {}
-        alt.kbConfig.map = alt.kbConfig.map or {}
-        alt.spell = alt.spell or {}
-        alt.macro = alt.macro or {}
-        alt.item = alt.item or {}
-
-        local dominos = select(4, GetAddOnInfo("Dominos"))
-        local bartender = select(4, GetAddOnInfo("Bartender4"))
-        local elvui = select(4, GetAddOnInfo("|cff1784d1ElvUI|r"))
-        if dominos then
-            alt.kbConfig.addon = "Dominos"
-        elseif bartender then
-            alt.kbConfig.addon = "Bartender"
-        elseif elvui then
-            alt.kbConfig.addon = "Elvui"
-        else
-            alt.kbConfig.addon = "Default"
-        end
     end
 end
 
@@ -71,68 +49,5 @@ function fs.UpdateAlt(_)
         alt.ridingSkill = 4
     elseif IsSpellKnown(90265) then
         alt.ridingSkill = 5
-    end
-end
-
-function fs.SpecScan(_)
-    local _, name = GetSpecializationInfo(GetSpecialization())
-    alt.kb[name] = {}
-    local numKeyBindings = GetNumBindings()
-    for j = 1, numKeyBindings do
-        local command = GetBinding(j)
-        local hasAction = string.find(command, "ACTION") or string.find(command, "Action")
-        local hasButton = string.find(command, "BUTTON") or string.find(command, "Button")
-        if hasAction and hasButton then
-            local keybind = GetBindingKey(command)
-            if keybind then
-                alt.kbConfig.map[command] = keybind
-            end
-        end
-    end
-    for i = 1, 120 do
-        local actionType, actionId, _ = GetActionInfo(i)
-        local nilCheck = GetActionTexture(i)
-        if nilCheck then
-            alt.kb[name][tostring(i)] = actionType .. ":" .. tostring(actionId)
-            if actionType == 'macro' then
-                local macroname,macroicon,macrobody = GetMacroInfo(actionId)
-                if macroname then
-                    alt.macro[tostring(actionId)] = {macroname, macroicon, macrobody}
-                end
-            elseif actionType == 'item' then
-                local itemname,_,_,_,_,itemtype,_,_,_,itemicon = GetItemInfo(actionId)
-                if itemname then
-                    alt.item[tostring(actionId)] = {itemname, itemicon, itemtype}
-                end
-            end
-        end
-    end
-    local _, specname = GetSpecializationInfo(GetSpecialization())
-    alt.spell[specname] = {}
-    alt.spell[specname]["base"] = {}
-    alt.spell[specname]["talent"] = {}
-    for i = 1, 3 do
-        local _,_,offset,numSpells = GetSpellTabInfo(i)
-        for j = offset + 1, offset + numSpells do
-            if not IsPassiveSpell(j, BOOKTYPE_SPELL) then
-                local spell,subspell,spellid = GetSpellBookItemName(j, BOOKTYPE_SPELL)
-                if spellid then
-                    local spelldesc = GetSpellDescription(spellid)
-                    local spellicon = GetSpellTexture(spellid)
-                    alt.spell[specname]["base"][spellid] = {spell, subspell, spelldesc, spellicon}
-                end
-            end
-        end
-    end
-    for i = 1, 7 do
-        for j = 1, 3 do
-            local _,spell,spellicon,_,_,spellid = GetTalentInfo(i, j, 1)
-            if spellid then
-                if not IsPassiveSpell(spellid) then
-                    local spelldesc = GetSpellDescription(spellid)
-                    alt.spell[specname]["talent"][spellid] = {spell, "", spelldesc, spellicon}
-                end
-            end
-        end
     end
 end
